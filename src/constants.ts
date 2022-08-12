04e438d09aaca2be9cb013e5cfa991d8ccdc3b8b
Charly POLY <cpoly55@gmail.com>
2022-08-13 01:11:40 +0200

    simple version

---
+++ src/constants.ts
@@ -0,0 +1,2 @@
+const DOMAIN = "";
+const PATH = "/push";
---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { fetch } from "@whatwg-node/fetch";
+
+interface Options {
+  apiToken: string;
+}
+
+let token: string;
+
+export default {
+  init: ({ apiToken }: Options) => {
+    token = apiToken;
+  },
+  push: (fn: string, args: Record<string, any>, _options: Options) => {
+    fetch(`${DOMAIN}/${PATH}`, {
+      body: JSON.stringify({
+        fn,
+        args,
+      }),
+      headers: {
+        "Content-type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+  },
+};
