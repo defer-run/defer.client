62d8c8e56e40f97da83666488ef5d5adee4cc885
Charly POLY <cpoly55@gmail.com>
2022-08-13 18:37:20 +0200

    Enter prerelease mode and version packages

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
-import { fetch } from "@whatwg-node/fetch";
+// import { fetch } from "@whatwg-node/fetch";
 
 interface Options {
   apiToken: string;
 }
 
-let token: string;
+// let token: string;
 
 export default {
-  init: ({ apiToken }: Options) => {
-    token = apiToken;
+  init: ({}: Options) => {
+    // token = apiToken;
   },
   push: (fn: string, args: Record<string, any>, _options: Options) => {
-    return fetch(`${DOMAIN}/${PATH}`, {
-      method: "POST",
-      body: JSON.stringify({
-        fn,
-        args,
-      }),
-      headers: {
-        "Content-type": "application/json",
-        Authorization: `Bearer ${token}`,
-      },
-    });
+    console.log(`cua.push(${fn}, ${args})`);
+    // return fetch(`${DOMAIN}/${PATH}`, {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     fn,
+    //     args,
+    //   }),
+    //   headers: {
+    //     "Content-type": "application/json",
+    //     Authorization: `Bearer ${token}`,
+    //   },
+    // });
   },
 };
