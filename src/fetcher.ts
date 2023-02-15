b523533c1441681ef4f2b9ce71c1f641668f7136
gearnode <bryan@frimin.fr>
2023-02-15 11:45:40 +0100

    Style

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,29 +1,27 @@
 import { fetch } from "@whatwg-node/fetch";
 import { URL } from "node:url";
 
-export interface DeferConfiguredFetcher {
-	(path: string, options: Partial<RequestInit>): ReturnType<typeof fetch>;
-}
+export type HTTPClient = (path: string, options: Partial<RequestInit>) => ReturnType<typeof fetch>;
 
 const basicAuth = (password: string) => {
 	const credentials = Buffer.from(`:${password}`).toString("base64")
 	return `Basic ${credentials}`
 }
 
-export function makeFetcher(
+export function makeHTTPClient(
 	apiEndpoint: string,
 	accessToken: string
-): DeferConfiguredFetcher {
-	return (path, pathOptions) => {
+): HTTPClient {
+	return (path, clientOptions) => {
 		const endpoint = new URL(apiEndpoint, path)
-		const customHeaderFields = pathOptions.headers || {}
+		const customHeaderFields = clientOptions.headers || {}
 		const options = {
 			headers: {
 				"Content-type": "application/json",
 				Authorization: basicAuth(accessToken),
 				...customHeaderFields,
 			},
-			...pathOptions,
+			...clientOptions,
 		}
 
 		return fetch(endpoint, options);
