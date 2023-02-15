16482e0bd4976f5410c05bd0f4753e2659cb152c
gearnode <bryan@frimin.fr>
2023-02-15 10:21:33 +0100

    Construct with URL instead of string concat

--- src/fetcher.ts
+++ src/fetcher.ts
@@ -1,22 +1,31 @@
 import { fetch } from "@whatwg-node/fetch";
+import { URL } from "node:url";
 
 export interface DeferConfiguredFetcher {
-  (path: string, options: Partial<RequestInit>): ReturnType<typeof fetch>;
+	(path: string, options: Partial<RequestInit>): ReturnType<typeof fetch>;
+}
+
+const basicAuth = (password: string) => {
+	const credentials = Buffer.from(`:${password}`).toString("base64")
+	return `Basic ${credentials}`
 }
 
 export function makeFetcher(
-  apiEndpoint: string,
-  apiToken: string
+	apiEndpoint: string,
+	accessToken: string
 ): DeferConfiguredFetcher {
-  return (path, options) =>
-    fetch(`${apiEndpoint}${path}`, {
-      headers: {
-        "Content-type": "application/json",
-        Authorization: `Basic ${Buffer.from(":" + apiToken).toString(
-          "base64"
-        )}`,
-        ...(options.headers || {}),
-      },
-      ...options,
-    });
+	return (path, pathOptions) => {
+		const endpoint = new URL(apiEndpoint, path)
+		const customHeaderFields = pathOptions.headers || {}
+		const options = {
+			headers: {
+				"Content-type": "application/json",
+				Authorization: basicAuth(accessToken),
+				...customHeaderFields,
+			},
+			...pathOptions,
+		}
+
+		return fetch(endpoint, options);
+	}
 }
