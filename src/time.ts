7d1b36c9381befc1751ffe6bdb3b3978bf79204f
gearnode <bryan@frimin.fr>
2023-02-23 13:48:32 -0800

    Remove unused file

--- src/time.ts
+++
@@ -1,5 +0,0 @@
-import parseDuration from "parse-duration";
-
-import { DelayString } from "./index.js";
-
-export const withDelay = (dt: Date, delay: DelayString): Date => new Date(dt.getTime() + parseDuration(delay));
