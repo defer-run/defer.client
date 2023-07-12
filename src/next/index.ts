10601acfcc814eba405a4fd394adb3e1dc8e320b
Charly POLY <cpoly55@gmail.com>
2023-07-12 11:25:44 +0200

    fix: ESM issues

--- src/next/index.ts
+++ src/next/index.ts
@@ -1,2 +1,2 @@
-export { DeferNextRoute, asNextRoute } from "./asNextRoute";
-export { UseDeferRoute, useDeferRoute } from "./useDeferRoute";
+export { DeferNextRoute, asNextRoute } from "./asNextRoute.js";
+export { UseDeferRoute, useDeferRoute } from "./useDeferRoute.js";
