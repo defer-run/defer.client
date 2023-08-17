99e19e24f0e6bfba553fa57951d4b00d0d9e696a
gearnode <bryan@frimin.fr>
2023-08-17 17:43:37 +0200

    Add force cancel execution support
    Signed-off-by: gearnode <bryan@frimin.fr>

--- src/cancelExecution.ts
+++ src/cancelExecution.ts
@@ -5,9 +5,10 @@ import {
 import { __httpClient } from "./index.js";
 
 export async function cancelExecution(
-  id: string
+  id: string,
+  force: boolean = false
 ): Promise<CancelExecutionResponse> {
-  if (__httpClient) return cancelExecution2(__httpClient, { id });
+  if (__httpClient) return cancelExecution2(__httpClient, { id, force });
 
   return {};
 }
