f1fe6d7abcfe6accd96f46a50693ff106f4a58a0
gearnode <bryan@frimin.fr>
2023-08-21 10:32:42 +0200

    Add httpClient simple test case
    Signed-off-by: gearnode <bryan@frimin.fr>

---
+++ tests/httpClient.spec.ts
@@ -0,0 +1,34 @@
+import { fetch } from "@whatwg-node/fetch";
+import { makeHTTPClient } from "../src/httpClient";
+
+jest.mock("@whatwg-node/fetch");
+const mockedFetch = fetch as jest.MockedFunction<typeof fetch>;
+
+const fakeEndpoint = "http://example.com";
+const fakeAccessToken = "foobar";
+const httpClient = makeHTTPClient(fakeEndpoint, fakeAccessToken);
+
+const expectedHeaderFields = {
+  Authorization: "Basic OmZvb2Jhcg==",
+  "Content-type": "application/json",
+  "User-Agent":
+    "defer/unknow (source: https://github.com/defer-run/defer.client)",
+};
+
+describe("makeHTTPClient/3", () => {
+  it("should not throw with 200", async () => {
+    const mockedResponse = new Response("{}", {
+      status: 200,
+    });
+    mockedFetch.mockResolvedValue(mockedResponse);
+    const response = await httpClient("GET", "/hello");
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith(fakeEndpoint + "/hello", {
+      method: "GET",
+      body: null,
+      headers: expectedHeaderFields,
+    });
+    expect(response).toEqual({});
+  });
+});
