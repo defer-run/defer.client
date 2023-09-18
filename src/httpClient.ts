7a887955bce9d095ba4b7b3f51a1e9bf4c398217
gearnode <bryan@frimin.fr>
2023-09-18 17:27:10 +0200

    WIP
    Signed-off-by: gearnode <bryan@frimin.fr>

--- src/httpClient.ts
+++ src/httpClient.ts
@@ -1,126 +1,124 @@
-import { URL, btoa, fetch } from "@whatwg-node/fetch";
-
 import {
-  APIError,
-  ClientError,
-  HTTPRequestError,
-  errorMessage,
+	APIError,
+	ClientError,
+	HTTPRequestError,
+	errorMessage,
 } from "./errors.js";
 import VERSION from "./version.js";
 
 export type HTTPClient = <T>(
-  method: string,
-  path: string,
-  body?: string | null
+	method: string,
+	path: string,
+	body?: string | null
 ) => Promise<T>;
 
 const basicAuth = (username: string, password: string) => {
-  const credentials = btoa(`${username}:${password}`);
-  return `Basic ${credentials}`;
+	const credentials = btoa(`${username}:${password}`);
+	return `Basic ${credentials}`;
 };
 
 interface APIErrorResponse {
-  error: string;
-  message: string;
+	error: string;
+	message: string;
 }
 
 export function makeHTTPClient(
-  apiEndpoint: string,
-  accessToken: string,
-  clientOptions?: RequestInit
+	apiEndpoint: string,
+	accessToken: string,
+	clientOptions?: RequestInit,
 ): HTTPClient {
-  return async <T>(
-    method: string,
-    path: string,
-    body: string | null = null
-  ): Promise<T> => {
-    let endpoint;
+	return async <T>(
+		method: string,
+		path: string,
+		body: string | null = null
+	): Promise<T> => {
+		let endpoint;
 
-    try {
-      endpoint = new URL(path, apiEndpoint);
-    } catch (error) {
-      let message;
+		try {
+			endpoint = new URL(path, apiEndpoint);
+		} catch (error) {
+			let message;
 
-      if (error instanceof Error)
-        message = `invalid endpoint url: ${errorMessage(error)}`;
-      else message = `unknown error: ${String(error)}`;
+			if (error instanceof Error)
+				message = `invalid endpoint url: ${errorMessage(error)}`;
+			else message = `unknown error: ${String(error)}`;
 
-      throw new ClientError(message);
-    }
-    const customHeaderFields = clientOptions?.headers || {};
-    const options: RequestInit = {
-      ...clientOptions,
-      method,
-      body,
-      headers: {
-        ...customHeaderFields,
-        "Content-type": "application/json",
-        "User-Agent": `defer/${VERSION} (source: https://github.com/defer-run/defer.client)`,
-        Authorization: basicAuth("", accessToken),
-      },
-    };
+			throw new ClientError(message);
+		}
+		const customHeaderFields = clientOptions?.headers || {};
+		const options: RequestInit = {
+			...clientOptions,
+			method,
+			body,
+			headers: {
+				...customHeaderFields,
+				"Content-type": "application/json",
+				"User-Agent": `defer/${VERSION} (source: https://github.com/defer-run/defer.client)`,
+				Authorization: basicAuth("", accessToken),
+			},
+		};
 
-    let response: Response;
-    let data: string;
+		let response: Response;
+		let data: string;
 
-    try {
-      response = await fetch(endpoint.toString(), options);
-    } catch (error) {
-      let message;
+		try {
+			response = await fetch(endpoint.toString(), options);
+		} catch (error) {
+			let message;
 
-      if (error instanceof Error)
-        message = `cannot execute http request: ${errorMessage(error)}`;
-      else message = `unknown error: ${String(error)}`;
+			if (error instanceof Error)
+				message = `cannot execute http request: ${errorMessage(error)}`;
+			else message = `unknown error: ${String(error)}`;
 
-      throw new ClientError(message);
-    }
+			throw new ClientError(message);
+		}
 
-    try {
-      data = await response.text();
-    } catch (error) {
-      let message;
+		try {
+			data = await response.text();
+		} catch (error) {
+			let message;
 
-      if (error instanceof Error)
-        message = `cannot decode http response: ${errorMessage(error)}`;
-      else message = `unknown error: ${String(error)}`;
+			if (error instanceof Error)
+				message = `cannot decode http response: ${errorMessage(error)}`;
+			else message = `unknown error: ${String(error)}`;
 
-      throw new Error(message);
-    }
+			throw new Error(message);
+		}
 
-    const { status } = response;
-    if (status >= 100 && status < 200) {
-      throw new ClientError("unexpected 1xx response code");
-    } else if (status >= 200 && status < 300) {
-      try {
-        return JSON.parse(data) as T;
-      } catch (error) {
-        const e = error as Error;
-        throw new ClientError(
-          `cannot decode http response body: ${errorMessage(e)}`
-        );
-      }
-    } else if (status >= 300 && status < 400) {
-      throw new ClientError("unexpected 3xx response code");
-    } else if (status >= 400 && status < 500) {
-      let decodedData: APIErrorResponse;
-      try {
-        decodedData = JSON.parse(data);
-      } catch (error) {
-        const e = error as Error;
-        throw new ClientError(
-          `cannot decode http response body: ${errorMessage(e)}`
-        );
-      }
+		const { status } = response;
+		if (status >= 100 && status < 200) {
+			throw new ClientError("unexpected 1xx response code");
+		} else if (status >= 200 && status < 300) {
+			try {
+				return JSON.parse(data) as T;
+			} catch (error) {
+				const e = error as Error;
+				throw new ClientError(
+					`cannot decode http response body: ${errorMessage(e)}`
+				);
+			}
+		} else if (status >= 300 && status < 400) {
+			throw new ClientError("unexpected 3xx response code");
+		} else if (status >= 400 && status < 500) {
+			let decodedData: APIErrorResponse;
+			try {
+				decodedData = JSON.parse(data);
+			} catch (error) {
+				const e = error as Error;
+				throw new ClientError(
+					`cannot decode http response body: ${errorMessage(e)}`
+				);
+			}
 
-      throw new APIError(decodedData.message, decodedData.error);
-    } else if (status >= 500 && status < 600) {
-      throw new HTTPRequestError("internal server error", status, data);
-    } else {
-      throw new HTTPRequestError(
-        `invalid http response status code: ${status}`,
-        status,
-        data
-      );
-    }
-  };
+			throw new APIError(decodedData.message, decodedData.error);
+		} else if (status >= 500 && status < 600) {
+			throw new HTTPRequestError("internal server error", status, data);
+		} else {
+			throw new HTTPRequestError(
+				`invalid http response status code: ${status}`,
+				status,
+				data
+			);
+		}
+	};
 }
