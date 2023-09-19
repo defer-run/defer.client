946da265d276d82529cca43a2cc60e1e561c2dc8
gearnode <bryan@frimin.fr>
2023-09-19 11:25:33 +0200

    Add utils file

---
+++ src/utils.ts
@@ -0,0 +1,17 @@
+// Although the function implementation may not be completely secure,
+// it is suitable for local use.
+export function randomUUID() {
+  return URL.createObjectURL(new Blob([])).slice(-36);
+}
+
+export function debug(...args: any) {
+  if (getEnv("DEFER_DEBUG")) {
+    console.debug(...args);
+  }
+}
+
+export function getEnv(key: string): string | undefined {
+  if (typeof process !== "undefined") return process.env[key];
+  if (typeof globalThis !== "undefined") return (globalThis as any)[key];
+  return;
+}
